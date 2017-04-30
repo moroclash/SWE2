5d5d45ba171b5b688456249b5470eca3c99ca384
TahaMgady <taha@taha-2.local>
2017-04-30 15:43:35 +0200

    @TahaMagdy: Statistics DONE

--- Back-End/BackEndSW2/src/system/Statistics.java
+++ Back-End/BackEndSW2/src/system/Statistics.java
@@ -1,21 +1,110 @@
+package system;
 
-import java.util.*;
+// @TahaMagdy: DONE
+// @Note: Any boolean function returns false 
+//        (Change it if you're implementing it)
+
+// Singletone Pattern
 
 /**
  * 
  */
 public class Statistics {
 
-    /**
-     * Default constructor
-     */
-    public Statistics() {
-    }
+
 
     /**
      * 
      */
     private double totalMoney;
+
+	public double getTotalMoney() {
+		return totalMoney;
+	}
+
+	public void setTotalMoney(double totalMoney) {
+		this.totalMoney = totalMoney;
+	}
+
+	public double getOurMoney() {
+		return ourMoney;
+	}
+
+	public void setOurMoney(double ourMoney) {
+		this.ourMoney = ourMoney;
+	}
+
+	public int getNumberOfEmployers() {
+		return numberOfEmployers;
+	}
+
+	public void setNumberOfEmployers(int numberOfEmployers) {
+		this.numberOfEmployers = numberOfEmployers;
+	}
+
+	public int getNumberOfFreelancers() {
+		return numberOfFreelancers;
+	}
+
+	public void setNumberOfFreelancers(int numberOfFreelancers) {
+		this.numberOfFreelancers = numberOfFreelancers;
+	}
+
+	public int getNumberOfAllTasks() {
+		return numberOfAllTasks;
+	}
+
+	public void setNumberOfAllTasks(int numberOfAllTasks) {
+		this.numberOfAllTasks = numberOfAllTasks;
+	}
+
+	public int getNumberOfAcceptedTasks() {
+		return numberOfAcceptedTasks;
+	}
+
+	public void setNumberOfAcceptedTasks(int numberOfAcceptedTasks) {
+		this.numberOfAcceptedTasks = numberOfAcceptedTasks;
+	}
+
+	public int getNumberOfRejectedTasks() {
+		return numberOfRejectedTasks;
+	}
+
+	public void setNumberOfRejectedTasks(int numberOfRejectedTasks) {
+		this.numberOfRejectedTasks = numberOfRejectedTasks;
+	}
+
+	public int getNumberOfComplaints() {
+		return numberOfComplaints;
+	}
+
+	public void setNumberOfComplaints(int numberOfComplaints) {
+		this.numberOfComplaints = numberOfComplaints;
+	}
+
+	public int getNumberOfAdmins() {
+		return numberOfAdmins;
+	}
+
+	public void setNumberOfAdmins(int numberOfAdmins) {
+		this.numberOfAdmins = numberOfAdmins;
+	}
+
+	public int getNumberOfBlockedPeople() {
+		return numberOfBlockedPeople;
+	}
+
+	public void setNumberOfBlockedPeople(int numberOfBlockedPeople) {
+		this.numberOfBlockedPeople = numberOfBlockedPeople;
+	}
+
+	public static Statistics getStatistics() {
+		return statistics;
+	}
+
+	public static void setStatistics(Statistics statistics) {
+		Statistics.statistics = statistics;
+	}
 
     /**
      * 
@@ -65,18 +154,10 @@ public class Statistics {
     /**
      * 
      */
-    private Statistics static statistics;
+    static private Statistics statistics;
 
 
 
-
-    /**
-     * @return
-     */
-    public void Statistics() {
-        // TODO implement here
-        return null;
-    }
 
     /**
      * 
@@ -88,7 +169,8 @@ public class Statistics {
     /**
      * @return
      */
-    public Statistics GetInstance() {
+    // أحا يا ياسر، أكتب أول حرف صغير مش كبير
+    public Statistics getInstance() {
         // TODO implement here
         return null;
     }
