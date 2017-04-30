9ee67592e09484f1af7e487f1a2050e891954e40
TahaMgady <taha@taha-2.local>
2017-04-30 16:19:25 +0200

    @TahaMagdy: Constraints DONE

--- Back-End/BackEndSW2/src/system/Constraints.java
+++ Back-End/BackEndSW2/src/system/Constraints.java
@@ -1,21 +1,84 @@
+package system;
 
-import java.util.*;
+// @TahaMagdy: DONE
+
+// Singletone Pattern
 
 /**
  * 
  */
 public class Constraints {
 
-    /**
-     * Default constructor
-     */
-    public Constraints() {
-    }
+
 
     /**
      * 
      */
     private int fr_timeoutPenalty;
+
+	public int getFr_timeoutPenalty() {
+		return fr_timeoutPenalty;
+	}
+
+	public void setFr_timeoutPenalty(int fr_timeoutPenalty) {
+		this.fr_timeoutPenalty = fr_timeoutPenalty;
+	}
+
+	public int getFr_cancelingTaskPenalty() {
+		return fr_cancelingTaskPenalty;
+	}
+
+	public void setFr_cancelingTaskPenalty(int fr_cancelingTaskPenalty) {
+		this.fr_cancelingTaskPenalty = fr_cancelingTaskPenalty;
+	}
+
+	public int getFr_overtimePenalty() {
+		return fr_overtimePenalty;
+	}
+
+	public void setFr_overtimePenalty(int fr_overtimePenalty) {
+		this.fr_overtimePenalty = fr_overtimePenalty;
+	}
+
+	public int getEm_rejectFinishedTaskPenalty() {
+		return em_rejectFinishedTaskPenalty;
+	}
+
+	public void setEm_rejectFinishedTaskPenalty(int em_rejectFinishedTaskPenalty) {
+		this.em_rejectFinishedTaskPenalty = em_rejectFinishedTaskPenalty;
+	}
+
+	public int getEm_cancelRunningTaskPenalty() {
+		return em_cancelRunningTaskPenalty;
+	}
+
+	public void setEm_cancelRunningTaskPenalty(int em_cancelRunningTaskPenalty) {
+		this.em_cancelRunningTaskPenalty = em_cancelRunningTaskPenalty;
+	}
+
+	public int getEm_cancelRunningTaskBudgetPenalty() {
+		return em_cancelRunningTaskBudgetPenalty;
+	}
+
+	public void setEm_cancelRunningTaskBudgetPenalty(int em_cancelRunningTaskBudgetPenalty) {
+		this.em_cancelRunningTaskBudgetPenalty = em_cancelRunningTaskBudgetPenalty;
+	}
+
+	public int getEm_rejectFinishedTaskBudgetPenalty() {
+		return em_rejectFinishedTaskBudgetPenalty;
+	}
+
+	public void setEm_rejectFinishedTaskBudgetPenalty(int em_rejectFinishedTaskBudgetPenalty) {
+		this.em_rejectFinishedTaskBudgetPenalty = em_rejectFinishedTaskBudgetPenalty;
+	}
+
+	public int getOurProfit() {
+		return ourProfit;
+	}
+
+	public void setOurProfit(int ourProfit) {
+		this.ourProfit = ourProfit;
+	}
 
     /**
      * 
@@ -42,6 +105,14 @@ public class Constraints {
      */
     private int em_cancelRunningTaskBudgetPenalty;
 
+	public static Constraints getConstrain() {
+		return constrain;
+	}
+
+	public static void setConstrain(Constraints constrain) {
+		Constraints.constrain = constrain;
+	}
+
     /**
      * 
      */
@@ -55,7 +126,7 @@ public class Constraints {
     /**
      * 
      */
-    private Constraints static constrain;
+    static  private Constraints constrain;
 
 
 
