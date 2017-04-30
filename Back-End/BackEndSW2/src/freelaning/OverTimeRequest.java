c79b3c317d75fef3e5eaae26f50fd02cbfb386e4
TahaMgady <taha@taha-2.local>
2017-04-30 15:19:56 +0200

    @TahaMagdy: OverTimeRequest DONE

--- Back-End/BackEndSW2/src/freelaning/OverTimeRequest.java
+++ Back-End/BackEndSW2/src/freelaning/OverTimeRequest.java
@@ -1,5 +1,9 @@
+package freelaning;
 
-import java.util.*;
+// @TahaMagdy: DONE
+
+import java.time.LocalDateTime;
+
 
 /**
  * 
@@ -16,6 +20,46 @@ public class OverTimeRequest {
      * 
      */
     private int hoursNeeded;
+
+	public int getHoursNeeded() {
+		return hoursNeeded;
+	}
+
+	public void setHoursNeeded(int hoursNeeded) {
+		this.hoursNeeded = hoursNeeded;
+	}
+
+	public Offer getOffer() {
+		return offer;
+	}
+
+	public void setOffer(Offer offer) {
+		this.offer = offer;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
 
     /**
      * 
@@ -35,7 +79,7 @@ public class OverTimeRequest {
     /**
      * 
      */
-    private DateTime date;
+    private LocalDateTime date;
 
 
 
